a4b59fb75f3a9d626cb8d17545c8e6e3caff4ffd
Edil2222 <zusupovaerkinai@mail.com>
2024-10-12 19:18:40 +0600

    start project

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Header.scss';
+
+const Header = () => {
+    return (
+        <header>
+            <div className="container">
+
+            </div>
+        </header>
+    );
+};
+
+export default Header;
