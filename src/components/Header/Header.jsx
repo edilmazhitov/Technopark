5a0192bf7ed26605f7e61ce80bc1838408068d43
Edil2222 <zusupovaerkinai@mail.com>
2024-10-12 21:35:09 +0600

    start v2

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,82 @@
 import React from 'react';
 import './Header.scss';
+import Logo from '../../assets/Logo.ico'
+import { MdHelpCenter } from "react-icons/md";
+import { MdOutlineLocalGroceryStore } from "react-icons/md";
+import { AiFillHeart } from "react-icons/ai";
+import { CiUser } from "react-icons/ci";
+import { MdOutlineInsertChartOutlined } from "react-icons/md";
 
 const Header = () => {
     return (
-        <header>
+        <header className='header'>
             <div className="container">
+                <div className="header__block">
+                    <div className="header__left">
+
+                        <img src={Logo} className='header__logo' alt=""/>
+                        <div role='button' className="header__catalog">
+                            <div className="header__catalog-burger"></div>
+                            <span>Каталог</span>
+                        </div>
+                        <input
+                            className='search'
+                            type="text"
+                            placeholder="Искать Товары"
+                        />
+                    </div>
+
+                    <ul className='header__list'>
+                        <li>
+                            <a href="" className="header__link">
+                            <MdOutlineInsertChartOutlined className='header__link-icons'/>
+                                <p>Сравнение</p>
+
+                            </a>
+                        </li>
+                        <li>
+                            <a href="" className="header__link">
+                                <AiFillHeart className='header__link-icons'/>
+                                <p>Избранное</p>
+
+                            </a>
+                        </li>
+                        <li>
+                            <a href="" className="header__link">
+                                <MdOutlineLocalGroceryStore className='header__link-icons'/>
+                                <p>Корзина</p>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="" className="header__link">
+                                <CiUser className='header__link-icons'/>
+                                <p>Войти</p>
+
+                            </a>
+                        </li>
+                    </ul>
+                    <div className="number">
+                        <div className="number__block">
+                            <div className="number__order">
+                                <a href="tel:88001008899">
+                                    8 (800) 100-88-99
+                                </a>
+                                <p>Оформить заказ</p>
+                            </div>
+                            <div className="number__support">
+                                <a href="tel:88001005588">
+                                    8 (800) 100-55-88
+                                </a>
+                                <p>
+                                    Служба поддержки
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                    <div role='button' className="header__info">
+                        <MdHelpCenter className='header__help'/>
+                    </div>
+                </div>
 
             </div>
         </header>
