16c88de185af03e77ebc0e4bf8ed107b97b2e9cf
Edil2222 <zusupovaerkinai@mail.com>
2024-11-09 15:14:38 +0600

    Delete Register/Login

--- src/routing/routing.jsx
+++ src/routing/routing.jsx
@@ -3,8 +3,6 @@ import Root from "../pages/Root/Root";
 import Home from "../pages/Home/Home";
 import Qyron from "../pages/Qyron/Qyron.jsx";
 import Smart from "../pages/Smart/Smart.jsx";
-import Register from "../pages/Register/Register.jsx";
-import Login from "../pages/Login/Login.jsx";
 import NotFound from "../pages/NotFound/NotFound.jsx";
 
 const router = createBrowserRouter([
@@ -23,14 +21,6 @@ const router = createBrowserRouter([
             {
                 path: 'smart',
                 element: <Smart/>
-            },
-            {
-                path: 'register',
-                element: <Register/>
-            },
-            {
-                path: 'login',
-                element: <Login/>
             },
             {
                 path: '*',
